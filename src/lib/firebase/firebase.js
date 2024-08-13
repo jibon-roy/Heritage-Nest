7b864382713859023b92dc987adad0e8851595e9
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-13 11:05:48 +0600

    firebase hosting added

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -1,11 +1,7 @@
 import getAuth from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
